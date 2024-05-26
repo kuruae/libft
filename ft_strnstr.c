191adb05f2c2f1fff20f6acd67ce4bf865e45e9f
Enzo <kurutsukipv@gmail.com>
2024-05-26 20:35:44 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: emagnani <emagnani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 10:33:36 by emagnani          #+#    #+#             */
-/*   Updated: 2024/05/21 10:51:56 by emagnani         ###   ########.fr       */
+/*   Updated: 2024/05/24 15:05:08 by emagnani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,27 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	size_t  j;
+	size_t	j;
+	size_t	k;
 
 	i = 0;
-	while (i < len)
+	k = 0;
+	if (!*little)
+		return ((char *)big);
+	while (i < len && big[i])
 	{
-		if (big[i] == little[i])
+		j = 0;
+		while (big[i] == little[j] && big[i] && little[j] &&i < len)
 		{
-			j = i;
-			while (i < (j + len))
-				i++;
-			if (big[i] == little[i])
-				return ((char *)&little[i]);
+			j++;
+			i++;
 		}
-		i++;
+		if (!little[j])
+			return ((char *)&big[i - ft_strlen(little)]);
+		k++;
+		i = k;
+		if (little[j] != big[i])
+			i++;
 	}
 	return (NULL);
 }
