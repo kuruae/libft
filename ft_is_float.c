5fa5e22981beee01ba3f282480b92598024606aa
Enzo <kurutsuipv@gmail.com>
2024-10-22 19:57:04 +0200

    quick fix

--- ft_is_float.c
+++ ft_is_float.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_float_or_int.c                               :+:      :+:    :+:   */
+/*   ft_is_float.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enzo <enzo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:37:17 by enzo              #+#    #+#             */
-/*   Updated: 2024/09/18 18:07:59 by enzo             ###   ########.fr       */
+/*   Updated: 2024/10/22 19:56:28 by enzo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ bool	ft_is_float(char *str)
 	bool	has_digit;
 	bool	has_dot;
 	bool	valid;
+	bool	second_valid;
 
 	has_digit = false;
 	has_dot = false;
 	valid = false;
-
 	if (!str || *str == '\0')
 		return (false);
 	if (*str == '-' || *str == '+')
@@ -41,7 +41,8 @@ bool	ft_is_float(char *str)
 	{
 		has_dot = true;
 		str++;
-		valid = check_float_part(&str, &has_digit, true) && valid;
+		second_valid = check_float_part(&str, &has_digit, true);
+		valid = valid && second_valid;
 	}
 	return (valid && has_digit && has_dot && *str == '\0');
 }
