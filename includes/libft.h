449e0593a36c7f85f0d54f841d89680cc048c914
Enzo Magnani <emagnani@MADE-f0Br5s8.clusters.42paris.fr>
2025-01-04 19:01:54 +0100

    added strjoinch

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: emagnani <emagnani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:33:04 by emagnani          #+#    #+#             */
-/*   Updated: 2024/10/23 17:27:09 by emagnani         ###   ########.fr       */
+/*   Updated: 2025/01/04 19:00:33 by emagnani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@ char	*ft_strdup(const char *source);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoinch(char *s1, char c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
